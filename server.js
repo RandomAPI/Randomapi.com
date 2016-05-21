d276a06d130905c4239a7de34be0fcea7f6d761b
Keith Armstrong <keith1015@gmail.com>
2016-05-21 14:27:37 -0500

    Refactored dashboard

--- server.js
+++ server.js
@@ -11,106 +11,53 @@ var grid = new contrib.grid({rows: 12, cols: 12, screen: screen});
 
 var server = require('./app').server;
 var app    = require('./app').app;
+
 var GeneratorForker = require('./api/0.1/GeneratorForker');
 
+// Global Generators
 Generators = {
-  basic:    new Array(5).fill().map(() => new GeneratorForker({execTime: 1, memory: 5, results: 25})),
-  standard: new Array(5).fill().map(() => new GeneratorForker({execTime: 5, memory: 10, results: 250})),
-  premium:  new Array(5).fill().map(() => new GeneratorForker({execTime: 10, memory: 25, results: 2500}))
+  basic:     new Array(1).fill().map(() => new GeneratorForker({execTime: 1, memory: 5, results: 25})),
+  standard:  new Array(2).fill().map(() => new GeneratorForker({execTime: 5, memory: 10, results: 250})),
+  premium:   new Array(3).fill().map(() => new GeneratorForker({execTime: 10, memory: 25, results: 2500})),
+  realtime:  new Array(3).fill().map(() => new GeneratorForker({execTime: 1, memory: 1, results: 1})),
+  speedtest: new Array(1).fill().map(() => new GeneratorForker({execTime: 5, memory: 5, results: 1000000}))
 };
 
-// var queueStats = _.each(Generators.basic, gen => {
-//   console.log(gen.queueLength());
-// });
+var types = ['basic', 'standard', 'premium'];
 
-var queueStats = {
+// For graphing stats
+var queueStats = {};
+var memStats = {};
 
+// Shared bar options
+var barOpts = {
+  barWidth: 3,
+  barSpacing: 1,
+  xOffset: 0,
+  maxHeight: 50
 };
 
-var basicBar = grid.set(0, 0, 2, 1, contrib.bar, {
-  label: 'Basic Queue',
-  barWidth: 3,
-  barSpacing: 1,
-  xOffset: 0,
-  maxHeight: 50,
-  barBgColor: 'red'
-});
-screen.append(basicBar) //must append before setting data
-setInterval(function() {
-  queueStats.basic = new Array(Generators.basic.length).fill().slice(0, 3).map((v, k) => Generators.basic[k].queueLength());
+var bars = {
+  basic: grid.set(0, 0, 2, 1, contrib.bar, _.merge(barOpts, {label: 'Basic Queue', barBgColor: 'red'})),
+  standard: grid.set(0, 1, 2, 1, contrib.bar, _.merge(barOpts, {label: 'Standard Queue', barBgColor: 'green'})),
+  premium: grid.set(0, 2, 2, 1, contrib.bar, _.merge(barOpts, {label: 'Premium Queue', barBgColor: 'cyan'}))
+};
 
-  basicBar.setData({
-    titles: new Array(Generators.basic.length).fill().slice(0, 3).map((v, k) => "#" + k),
-    data: queueStats.basic
-  });
-  screen.render();
-}, 250);
+_.each(bars, bar => screen.append(bar));
 
-var standardBar = grid.set(0, 1, 2, 1, contrib.bar, {
-  label: 'Standard Queue',
-  barWidth: 3,
-  barSpacing: 1,
-  xOffset: 0,
-  maxHeight: 50,
-  barBgColor: 'green'
-});
-screen.append(standardBar) //must append before setting data
-setInterval(function() {
-  queueStats.standard = new Array(Generators.standard.length).fill().slice(0, 3).map((v, k) => Generators.standard[k].queueLength());
-
-  standardBar.setData({
-    titles: new Array(Generators.standard.length).fill().slice(0, 3).map((v, k) => "#" + k),
-    data: queueStats.standard
-  });
-  screen.render()
-}, 100);
-
-var premiumBar = grid.set(0, 2, 2, 1, contrib.bar, {
-  label: 'Premium Queue',
-  barWidth: 3,
-  barSpacing: 1,
-  xOffset: 0,
-  maxHeight: 50,
-  barBgColor: 'cyan'
-});
-screen.append(premiumBar) //must append before setting data
-setInterval(function() {
-  queueStats.premium = new Array(Generators.premium.length).fill().slice(0, 3).map((v, k) => Generators.premium[k].queueLength());
-
-  premiumBar.setData({
-    titles: new Array(Generators.premium.length).fill().slice(0, 3).map((v, k) => "#" + k),
-    data: queueStats.premium
-  });
-  screen.render()
-}, 100);
-
-var basicTable =  grid.set(0, 3, 2, 1, contrib.table, {
-  fg: 'red',
-  label: 'Basic Generators',
-  columnSpacing: 1,
-  interactive: false,
-  columnWidth: [4, 8, 3],
-});
-
-var standardTable =  grid.set(0, 4, 2, 1, contrib.table, {
-  fg: 'green',
-  label: 'Standard Generators',
+var tableOpts = {
   columnSpacing: 1,
   interactive: false,
   columnWidth: [4, 8, 3]
-});
+};
 
-var premiumTable =  grid.set(0, 5, 2, 1, contrib.table, {
-  fg: 'cyan',
-  label: 'Premium Generators',
-  columnSpacing: 1,
-  interactive: false,
-  columnWidth: [4, 8, 3]
-});
+var tables = {
+  basic: grid.set(0, 3, 2, 1, contrib.table, _.merge(tableOpts, {fg: 'red', label: 'Basic Generators'})),
+  standard: grid.set(0, 4, 2, 1, contrib.table, _.merge(tableOpts, {fg: 'green', label: 'Standard Generators'})),
+  premium: grid.set(0, 5, 2, 1, contrib.table, _.merge(tableOpts, {fg: 'cyan', label: 'Premium Generators'})),
+};
 
-//set dummy data for table
-function generateTable() {
-  var types = ['basic', 'standard', 'premium'];
+function generateTables() {
   var data = {
     basic: [],
     standard: [],
@@ -123,21 +70,18 @@ function generateTable() {
       row.push(i);
       row.push(Generators[types[j]][i].totalJobs());
       row.push(Generators[types[j]][i].memUsage());
-
       data[types[j]].push(row);
     }
   }
 
-   basicTable.setData({headers: ['#', 'Jobs', 'Mem'], data: data.basic})
-   standardTable.setData({headers: ['#', 'Jobs', 'Mem'], data: data.standard})
-   premiumTable.setData({headers: ['#', 'Jobs', 'Mem'], data: data.premium})
+  _.each(tables, (table, val) => {
+    table.setData({headers: ['#', 'Jobs', 'Mem'], data: data[val]});
+  });
 }
 
-generateTable();
-setInterval(generateTable, 1000)
+generateTables();
 
-
-var totalQueues = grid.set(4, 0, 4, 12, contrib.line, {
+var totalQueues = grid.set(4, 0, 3, 6, contrib.line, {
   showNthLabel: 5,
   label: 'Total Queues',
   showLegend: false,
@@ -157,8 +101,97 @@ var botline = { title: 'botline', style: {line: 'white'}, x:[], y: [] };
 totalQueues.setData([botline, basicStats, standardStats, premiumStats]);
 
 var time = Math.floor(new Date().getTime()/1000);
+var elapsed;
+
+log = grid.set(0, 6, 2, 6, contrib.log, {
+  fg: "white",
+  label: 'Server Log'
+});
+
+var loadOpts = {
+  radius: 10,
+  arcWidth: 4,
+  yPadding: 2
+};
+
+var loads = {
+  basic: grid.set(2, 0, 2, 1, contrib.donut, _.merge(loadOpts, {label: 'Basic Load', data: [{label: 'Basic Load', percent: 0}]})),
+  standard: grid.set(2, 1, 2, 1, contrib.donut, _.merge(loadOpts, {label: 'Standard Load', data: [{label: 'Standard Load', percent: 0}]})),
+  premium: grid.set(2, 2, 2, 1, contrib.donut, _.merge(loadOpts, {label: 'Premium Load', data: [{label: 'Premium Load', percent: 0}]})),
+  overall: grid.set(2, 3, 2, 1, contrib.donut, _.merge(loadOpts, {label: 'Overall Load', data: [{label: 'Overall Load', percent: 0}]})),
+};
+
+var instructions = grid.set(11, 0, 1, 12, contrib.markdown, {
+  markdown: "**^G** Manual Garbage Collection\n**^Q** Quit"
+});
+
+function updateDonut() {
+  var overallPerc = 0;
+
+  _.each(loads, (load, name) => {
+    if (name === 'overall') return;
+
+    var percent = _.sum(queueStats[name])/(Generators[name].length * 50) * 100 || 0;
+    var color = "green";
+    if (percent >= 50) color = "cyan";
+    if (percent >= 75) color = "yellow";
+    if (percent >= 90) color = "red";
+    load.setData([
+       {percent: percent, label: '', 'color': color}
+    ]);
+    overallPerc += percent;
+  });
+
+  overallPerc /= 3;
+  var color = "green";
+  if (overallPerc >= 50) color = "cyan";
+  if (overallPerc >= 75) color = "yellow";
+  if (overallPerc >= 90) color = "red";
+
+  loads.overall.setData([
+    {percent: Math.floor(overallPerc), label: 'Overall Load', 'color': color}
+  ]);
+}
+
+// Hotkeys
+
+screen.key(['C-q'], function(ch, key) {
+  return process.exit(0);
+});
+
+screen.key(['C-g'], function(ch, key) {
+  logger("Emitting Manual Garbage Collection Event");
+  Generators.basic.forEach(gen => gen.gc());
+  Generators.standard.forEach(gen => gen.gc());
+  Generators.premium.forEach(gen => gen.gc());
+});
+
+//////////
+
+screen.render();
+
+// Intervals
+
+// Queue length stats
 setInterval(function() {
-  var fmt   = moment.duration(Math.floor(new Date().getTime()/1000) - time, 'seconds');
+  types.forEach(type => {
+    queueStats[type] = new Array(Generators[type].length).fill().slice(0, 3).map((v, k) => Generators[type][k].queueLength());
+    bars[type].setData({
+      titles: new Array(Generators[type].length).fill().slice(0, 3).map((v, k) => "#" + k),
+      data: queueStats[type]
+    });
+  });
+
+  screen.render();
+}, 250);
+
+// Generator Tables
+setInterval(generateTables, 1000)
+
+// Queue Chart
+setInterval(function() {
+  elapsed = Math.floor(new Date().getTime()/1000) - time;
+  var fmt   = moment.duration(elapsed, 'seconds');
   var hours = Math.floor(fmt.asHours());
   var min   = Math.floor(fmt.asMinutes());
   var sec   = fmt.asSeconds() - Math.floor(fmt.asMinutes()) * 60;
@@ -173,94 +206,34 @@ setInterval(function() {
   if (botline.x.length > 25) {
     botline.x.shift();
     botline.y.shift();
+
     basicStats.y.shift();
     standardStats.y.shift();
     premiumStats.y.shift();
   }
   totalQueues.setData([botline, basicStats, standardStats, premiumStats]);
-  screen.render()
+  screen.render();
+
+  if (elapsed % 3600 === 0) {
+    logger("Emitting Hourly Garbage Collection Event");
+    Generators.basic.forEach(gen => gen.gc());
+    Generators.standard.forEach(gen => gen.gc());
+    Generators.premium.forEach(gen => gen.gc());
+  }
 }, 1000)
 
-log = grid.set(2, 4, 2, 2, contrib.log, {
-  fg: "white",
-  label: 'Server Log'
-});
-
-var loadBasic = grid.set(2, 0, 2, 1, contrib.donut,
-  {
-  label: 'Basic Load',
-  radius: 10,
-  arcWidth: 4,
-  yPadding: 2,
-  data: [{label: 'Basic Load', percent: 0}]
-});
-
-var loadStandard = grid.set(2, 1, 2, 1, contrib.donut,
-  {
-  label: 'Standard Load',
-  radius: 10,
-  arcWidth: 4,
-  yPadding: 2,
-  data: [{label: 'Standard Load', percent: 0}]
-});
-
-var loadPremium = grid.set(2, 2, 2, 1, contrib.donut,
-  {
-  label: 'Premium Load',
-  radius: 10,
-  arcWidth: 4,
-  yPadding: 2,
-  data: [{label: 'Premium Load', percent: 0}]
-});
-
-var overallLoad = grid.set(2, 3, 2, 1, contrib.donut,
-  {
-  label: 'Overall Load',
-  radius: 10,
-  arcWidth: 4,
-  yPadding: 2,
-  data: [{label: 'Overall Load', percent: 0}]
-});
-
-function updateDonut() {
-  var overallPerc = 0;
-  var donuts = [loadBasic, loadStandard, loadPremium];
-  var types = ['basic', 'standard', 'premium'];
-  _.each(types, (load, num) => {
-    var percent = _.sum(queueStats[load])/(Generators[load].length * 50) * 100 || 0;
-    var color = "green";
-    if (percent >= 50) color = "cyan";
-    if (percent >= 75) color = "yellow";
-    if (percent >= 90) color = "red";
-    donuts[num].setData([
-       {percent: percent, label: '', 'color': color}
-    ]);
-    overallPerc += percent;
-  });
-
-  overallPerc /= 3;
-  var color = "green";
-  if (overallPerc >= 50) color = "cyan";
-  if (overallPerc >= 75) color = "yellow";
-  if (overallPerc >= 90) color = "red";
-
-  overallLoad.setData([
-    {percent: Math.floor(overallPerc), label: 'Overall Load', 'color': color}
-  ]);
-}
-
+// Donut Loads
 setInterval(function() {   
    updateDonut();
    screen.render()
 }, 250)
 
-screen.key(['escape', 'q', 'C-c'], function(ch, key) {
-  return process.exit(0);
-});
-screen.render();
+////////////
+
+
+
 
 startServer();
-
 function startServer() {
   server.listen(app.get('port'));
   server.on('error', error => {
@@ -284,7 +257,7 @@ function startServer() {
     var bind = typeof addr === 'string'
       ? 'pipe ' + addr
       : 'port ' + addr.port;
-    log.log('Listening on ' + bind);
+    logger('Listening on ' + bind);
   });
   
 
@@ -307,4 +280,8 @@ function pad(n, width, z) {
   z = z || '0';
   n = n + '';
   return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+}
+
+function logger(msg) {
+  log.log(moment().format('LTS') + " - " + msg);
 }
