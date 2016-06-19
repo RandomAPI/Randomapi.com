58c84f209718430a63df1557a47799a2f12ed12b
Keith Armstrong <keith1015@gmail.com>
2016-06-19 11:50:58 -0500

    Distribute requests to genreators based on tier

--- routes/api.js
+++ routes/api.js
@@ -2,27 +2,29 @@ const express = require('express');
 const router  = express.Router();
 const logger  = require('../utils').logger;
 const spawn   = require('child_process').spawn;
+const User    = require('../models/User');
 
 router.get('/:ref?', (req, res, next) => {
   const Generators = req.app.get('Generators');
   let type = Math.floor(Math.random() * 100);
-
-  if (type < 33) {
-    type = 'premium';
-  } else if (type < 66) {
-    type = 'standard';
-  } else {
-    type = 'basic';
-  }
-
-  let shortest = Math.floor(Math.random() * Generators[type].length);
-  for (let i = 0; i < Generators[type].length; i++) {
-    if (Generators[type][i].queueLength() < Generators[type][shortest].queueLength()) {
-      shortest = i;
+  User.getCond({apikey: req.query.key}).then(user => {
+    if (user.tier === 1) {
+      type = 'basic';
+    } else if (user.tier === 2) {
+      type = 'standard';
+    } else if (user.tier === 3) {
+      type = 'premium';
     }
-  }
 
-  Generators[type][shortest].queue.push({req, res});
+    let shortest = Math.floor(Math.random() * Generators[type].length);
+    for (let i = 0; i < Generators[type].length; i++) {
+      if (Generators[type][i].queueLength() < Generators[type][shortest].queueLength()) {
+        shortest = i;
+      }
+    }
+
+    Generators[type][shortest].queue.push({req, res});
+  });
 });
 
 module.exports = router;
