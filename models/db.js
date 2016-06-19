4bc3b2f5eb71aa45bec8a41d105abd4fe6cd288f
Keith Armstrong <keith1015@gmail.com>
2016-06-19 09:52:20 -0500

    Add socket option for MySQL connection

--- models/db.js
+++ models/db.js
@@ -3,10 +3,11 @@ const settings = require('../settings.json');
 const logger   = require('../utils').logger;
 
 const connection = mysql.createConnection({
-  host:     settings.db.host,
-  database: settings.db.database,
-  user:     settings.db.username,
-  password: settings.db.password
+  host:       settings.db.host,
+  socketPath: settings.db.socketPath,
+  database:   settings.db.database,
+  user:       settings.db.username,
+  password:   settings.db.password
 });
 
 connection.connect(err => {
@@ -16,5 +17,10 @@ connection.connect(err => {
   }
   logger('[db]: connected as id ' + connection.threadId);
 });
+
+// Keep connection alive
+setInterval(function () {
+    connection.query('SELECT 1');
+}, settings.db.keepAliveInterval);
 
 module.exports = connection;
