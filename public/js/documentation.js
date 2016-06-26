37681c1032b70c3ca1782e8a6b018d68fc07ea53
Keith Armstrong <keith1015@gmail.com>
2016-06-26 17:58:13 -0500

    Fix broken documentation content scrolling

--- public/js/documentation.js
+++ public/js/documentation.js
@@ -1,6 +1,6 @@
 $(() => {
   hljs.initHighlightingOnLoad();
-  $('a[href*=\\#]:not([href=\\#])').click(() => {
+  $('a[href*=\\#]:not([href=\\#])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       let target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
