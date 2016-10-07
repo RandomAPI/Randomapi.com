f628c6034bebfbf5352ca2923dcbbffcb2d24d7e
Keith Armstrong <keith1015@gmail.com>
2016-10-07 00:45:03 -0500

    Mobile friendly subnav

--- src/js/style.js
+++ src/js/style.js
@@ -6,9 +6,37 @@ $(() => {
     if (document.body.clientWidth <= 900) {
       $('.tripleSection').removeClass("column");
       $('.freeLogo').removeClass("invisible");
+      $('#mobileSubNavMenu').css('cursor', 'pointer')
+      $('#mobileSubNav').removeClass("invisible");
+      $('#mobileSubNavMenu').on('click', () => mobileDropDown());
+      $("#mobileSubNavArrow").addClass('arrow-down')
     } else {
       $('.tripleSection').addClass("column");
       $('.freeLogo').addClass("invisible");
+      $('#mobileSubNavMenu').css('cursor', '')
+      $('#mobileSubNav').addClass("invisible");
+      $('#mobileSubNavMenu').unbind('click');
+      $("#mobileSubNavArrow").removeClass('arrow-down');
+    }
+  }
+
+  // Close the dropdown menu if the user clicks outside of it
+  window.onclick = function(event) {
+    if (!event.target.matches('#mobileSubNavMenu')) {
+
+      var dropdowns = document.getElementsByClassName("dropdown-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        $('#dropdown').removeClass("show");
+      }
     }
   }
 });
+
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+
+function mobileDropDown() {
+  $('#dropdown').addClass("show");
+}
