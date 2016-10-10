7414e09ffb5283a7a62e99d568f052232f8eedde
Keith Armstrong <keith1015@gmail.com>
2016-10-10 15:29:59 -0500

    Improve homepage layout, addusername verification, and add quick new links for /view pages

--- src/js/demoEditor.js
+++ src/js/demoEditor.js
@@ -1,3 +1,7 @@
+$(() => {
+  newExample();
+});
+
 socket = io($('server').html());
 
 let typingTimer, lastAbuse;
@@ -65,7 +69,7 @@ api.email = list([
 
 api.address = faker.address.streetAddress();
 
-// Created 30 - 900 days ago
+// Created 30 - 3600 days ago
 let created = timestamp() - 86400 * random.numeric(30, 3600);
 api.created = moment(created * 1000).format('LL');
 
@@ -84,4 +88,122 @@ function randomEmail() {
 }`, -1);
   lintCode();
   });
+}
+
+
+function newExample() {
+  let samples = [
+`// Example of a random customer generator
+const faker = require('faker'); // Faker.js
+
+api.customer  = {
+    id:    random.special(4, 8),
+    name:  faker.name.findName(),
+    phone: faker.phone.phoneNumber("(###) ###-####"),
+    address: {
+        street: faker.address.streetAddress(),
+        city: faker.address.city(),
+        state: faker.address.state(),
+    }
+};`,
+
+`// Example of a random user purchase invoice
+const moment = require('moment'); // moment.js to format dates
+
+// Generate fake credit card numbers
+const cc = require('keith/Credit Card Generator/2');
+
+// User defined code to generate invoice numbers
+api.invoiceID = require('keith/invoice number generator/1')();
+api.date = moment().format('LLL');
+
+// Inline list of products to choose from
+let products = {
+    milk: 229, eggs: 100, bread: 243, butter: 200,
+    juice: 369, cereal: 312, poptarts: 371, sprite: 1077,
+    mentos: 330, beer: 945, apple: 75, avocado: 85
+};
+
+api.itemsPurchased = random.numeric(1, 10);
+api.items = [];
+
+let total = 0;
+for (let i = 0; i < api.itemsPurchased; i++) {
+    let item = list(products);
+    api.items.push(item);
+    total += products[item];
+}
+api.items = api.items.toString();
+api.card  = cc('VISA').toString().match(/.{4}/g).join('-');
+api.total = \`$\${total/100}\`;`,
+
+`// Example of a random user generator
+const faker = require('faker'); // Faker.js library
+
+// Custom user snippet to generate formatted phone numbers
+const phonenum = require('keith/phonenum/1');
+
+api.company  = faker.company.companyName();
+api.username = faker.internet.userName();
+api.password = faker.internet.password();
+api.role     = list(['guest', 'user', 'administrator']);
+api.phone    = phonenum();
+api.cell     = phonenum(\`(\${list(['214', '469', '972'])}) xxx-xxxx\`);`,
+
+`// Geocaching API demo from http://blog.randomapi.com/geocaching-api-demo/
+const faker  = require('faker'); // Faker.js library
+const moment = require('moment');
+
+api.name       = trailname();
+api.username   = faker.internet.userName();
+api.rating     = random.numeric(0, 50) / 10;
+api.favorites  = Math.ceil(api.rating * random.numeric(1, 15));
+api.visits     = api.favorites * random.numeric(1, 15)
+api.difficulty = random.numeric(1, 5);
+api.terrain    = random.numeric(1, 5);
+api.size       = list(['mini', 'small', 'medium', 'big', 'large']);
+
+// Created 30 - 900 days ago
+let created = timestamp() - 86400 * random.numeric(30, 900);
+
+// Moment accepts timestamps in milliseconds
+api.created = moment(created * 1000).format('LL');
+
+// Updated date will be before the present but after the creation date
+let updated = timestamp() - random.numeric(0, timestamp() - created);
+api.updated = moment(updated * 1000).format('LL');
+
+api.coords = \`\${faker.address.latitude()} \${faker.address.longitude()}\`;
+
+// Snippet code
+function trailname() {
+    let trails     = ["trail", "path", "route", "stream", "walkway", "beaten path", "footpath"]
+    let adjectives = ["dusty", "old", "scenic", "historic", "shady", "sunny"];
+    let colors     = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+
+    let combos = [
+        [colors, trails],
+        [adjectives, trails],
+        [colors, adjectives, trails]
+    ];
+
+    let trailName = "";
+    list(combos).forEach(part => trailName += " " + capitalize(list(part)));
+
+    return trailName.trim();
+
+    function capitalize(str) {
+        return str.charAt(0).toUpperCase() + str.slice(1);
+    }
+}`];
+
+  let currentSample = Math.floor(Math.random()*samples.length);
+
+  editor.setValue(samples[++currentSample%samples.length], -1);
+  lintCode();
+
+  newExample = () => {
+    editor.setValue(samples[++currentSample%samples.length], -1);
+    lintCode();
+  };
 }
--- src/js/nameLimits.js
+++ src/js/nameLimits.js
@@ -1,5 +1,5 @@
 $(() => {
-  let limitsBad = false, tagsBad = false;
+  let limitsBad = false, tagsBad = false, usernameBad = false;
   $('#limitsInput').on('input', function() {
 
     limitsBad = $(this).val().match(/^[a-zA-Z0-9 _\-\.+\[\]\{\}\(\)]{0,32}$/) === null
@@ -10,6 +10,11 @@ $(() => {
     let tags = $(this).val().split(',').map(tag => tag.trim()).filter(tag => tag !== "");
     let rejects = tags.filter(tag => tag.match(/^([a-zA-Z0-9 _\-\.+\[\]\{\}\(\)]{0,32})$/g) === null);
     tagsBad = rejects.length > 0;
+    badCheck();
+  });
+
+  $('#usernameInput').on('input', function() {
+    usernameBad = $(this).val().match(/^[A-z0-9]{1,20}$/) === null && $(this).val() !== '';
     badCheck();
   });
 
@@ -30,6 +35,14 @@ $(() => {
       $("#submit").prop('disabled', true);
     } else {
       $("#submit").prop('disabled', false);
+    }
+
+    if (usernameBad) {
+      $('#usernameLimits').show();
+      $("#registerButton").prop('disabled', true);
+    } else {
+      $('#usernameLimits').hide();
+      $("#registerButton").prop('disabled', false);
     }
   }
 });
